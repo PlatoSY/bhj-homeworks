3ab0bf2994672d319634b077b373ecac784d123f
PlatoSY <sergeplato@gmail.com>
2022-09-13 11:44:55 +0300

    countdown v1

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,1 +1,31 @@
-ddfdf;
+let elementTimer = document.getElementById('timer');
+let timerCounter = setInterval(counter, 1000);
+function counter() {
+  elementTimer.textContent--;
+  if (elementTimer.textContent <= 0) {
+    clearInterval(timerCounter);
+    alert('Вы победили в конкурсе!');
+  }
+}
+// let timerNumber = Number(elementTimer.textContent);
+// let date = new Date();
+// let h = date.getHours();
+// let m = date.getMinutes();
+// let s = date.getSeconds();
+// let dateString = h + ':' + m + ':' + s;
+// elementTimer.textContent = dateString;
+
+// let timerCounter = setInterval(counter, 1000);
+// function counter() {
+//   s--;
+//   if (s == 0) {
+//     clearInterval(timerCounter);
+//     alert('Вы победили в конкурсе!');
+//   }
+// }
+// let h = '00';
+// let m = '00';
+// let s = elementTimer.textContent;
+// elementTimer.textContent = `${h}:${m}:${s}`;
+// let countDown = new Date();
+// elementTimer.textContent = countDown;
