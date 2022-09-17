f743f113e9901cebd0a738ad3124058fc162f4f0
PlatoSY <sergeplato@gmail.com>
2022-09-17 11:03:03 +0300

    bhj-2.1 V1

---
+++ element-search/popups/task.js
@@ -0,0 +1,20 @@
+const closeButtons = document.querySelectorAll('.modal__close');
+const modalMain = document.getElementById('modal_main');
+const showSuccess = document.querySelector('.show-success');
+const modalSuccess = document.getElementById('modal_success');
+
+let arrClose = Array.from(closeButtons);
+function modalClose() {
+  arrClose.forEach(
+    (i) =>
+      (i.onclick = function () {
+        modalMain.classList.remove('modal_active');
+      })
+  );
+}
+modalClose();
+
+showSuccess.onclick = function () {
+  modalMain.classList.remove('modal_active');
+  modalSuccess.classList.add('modal_active');
+};
