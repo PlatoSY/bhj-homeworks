34d4adf96986e35ed2a55441305fa1a9d21b1c93
PlatoSY <sergeplato@gmail.com>
2023-01-23 18:16:14 +0300

    правки  html-forms 1.2

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,6 +1,25 @@
-let itemsCheckboxes = Array.from(document.querySelectorAll('.interest__check'));
-console.log(itemsCheckboxes);
-// itemsCheckboxes = itemsCheckboxes.checked = true;
-// let labels = Array.from(document.querySelectorAll('label'));
+const checkboxes = Array.from(document.querySelectorAll('.interest__check'));
 
-// console.log(labels);
+let topCheckBoxes = [];
+
+checkboxes.forEach((item) => {
+  if (!item.closest('.interests_active')) {
+    topCheckBoxes.push(item);
+  }
+});
+
+topCheckBoxes.forEach((item) => {
+  item.addEventListener('change', () => {
+    const containerForCheckBoxesGroup = item.closest('.interest');
+    const ulContainer = containerForCheckBoxesGroup.querySelector('ul');
+    const innerCheckBoxes = Array.from(ulContainer.querySelectorAll('input'));
+
+    innerCheckBoxes.forEach((elem) => {
+      if (!elem.checked) {
+        elem.checked = true;
+      } else {
+        elem.checked = false;
+      }
+    });
+  });
+});
